74c1a26539dea67b6c46e2473a8adeade8d13421
DaniilAev <daniilaev415@gmail.com>
2026-07-19 22:34:02 +0300

    edited dist

--- buffer_funcs.c
+++ buffer_funcs.c
@@ -30,22 +30,38 @@ int fill_buffer(char buffer[], int size){
     return position;
 }
 
-void dist(char ch_buffer[], double opn_buffer[], char opr_buffer[], int code){
+vo dist(char ch_buffer[], double opn_buffer[], char opr_buffer[], int code){
     int ch_car = 0;
     int opn_car = 0;
     int opr_car = 0;
     while (ch_car < code)
     {
-        if (ch_buffer[ch_car] = ' '){ /*Space case*/
+        if (ch_buffer[ch_car] == ' '){ /*Space case*/
             ++ch_car;
             continue;
         }
-
-        if (ch_buffer[ch_car] == '+' && (ch_buffer[ch_car+1] == ' ' || ch_buffer[ch_car+1] == '\0')){
+        if (ch_buffer[ch_car] == '+' && (ch_buffer[ch_car+1] == ' ' || ch_buffer[ch_car+1] == '\0')){ /*Plus case*/
             opr_buffer[opr_car++] = '+';
             ch_car += 2;
             continue;
         }
+        if (ch_buffer[ch_car] == '-' && (ch_buffer[ch_car+1] == ' ' || ch_buffer[ch_car+1] == '\0')){ /* Minus case*/
+            opr_buffer[opr_car++] = '-';
+            ch_car += 2;
+            continue;
+        }
+        if (ch_buffer[ch_car] == '*' && (ch_buffer[ch_car+1] == ' ' || ch_buffer[ch_car+1] == '\0')){ /* Mult case*/
+            opr_buffer[opr_car++] = '*';
+            ch_car += 2;
+            continue;
+        }
+        if (ch_buffer[ch_car] == '/' && (ch_buffer[ch_car+1] == ' ' || ch_buffer[ch_car+1] == '\0')){ /* Div case*/
+            opr_buffer[opr_car++] = '/';
+            ch_car += 2;
+            continue;
+        }
+        
     }
+    return 0;
     
 }
