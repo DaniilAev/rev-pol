2d97042f3463f6aa015f3ebfc684d750f1b35cc9
DaniilAev <daniilaev415@gmail.com>
2026-07-19 22:49:58 +0300

    edited dist

--- buffer_funcs.c
+++ buffer_funcs.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int valid(char s[]);
+
 void clean_buffer(char char_buffer[], int char_size, double operand_buffer[], int operand_size, char operator_buffer[], int operator_size){
        int position;
        for (position = 0; position < char_size; ++position)
@@ -30,7 +32,7 @@ int fill_buffer(char buffer[], int size){
     return position;
 }
 
-vo dist(char ch_buffer[], double opn_buffer[], char opr_buffer[], int code){
+int dist(char ch_buffer[], double opn_buffer[], char opr_buffer[], int code){
     int ch_car = 0;
     int opn_car = 0;
     int opr_car = 0;
@@ -40,27 +42,33 @@ vo dist(char ch_buffer[], double opn_buffer[], char opr_buffer[], int code){
             ++ch_car;
             continue;
         }
-        if (ch_buffer[ch_car] == '+' && (ch_buffer[ch_car+1] == ' ' || ch_buffer[ch_car+1] == '\0')){ /*Plus case*/
+        else if (ch_buffer[ch_car] == '+' && (ch_buffer[ch_car+1] == ' ' || ch_buffer[ch_car+1] == '\0')){ /*Plus case*/
             opr_buffer[opr_car++] = '+';
             ch_car += 2;
             continue;
         }
-        if (ch_buffer[ch_car] == '-' && (ch_buffer[ch_car+1] == ' ' || ch_buffer[ch_car+1] == '\0')){ /* Minus case*/
+        else if (ch_buffer[ch_car] == '-' && (ch_buffer[ch_car+1] == ' ' || ch_buffer[ch_car+1] == '\0')){ /* Minus case*/
             opr_buffer[opr_car++] = '-';
             ch_car += 2;
             continue;
         }
-        if (ch_buffer[ch_car] == '*' && (ch_buffer[ch_car+1] == ' ' || ch_buffer[ch_car+1] == '\0')){ /* Mult case*/
+        else if (ch_buffer[ch_car] == '*' && (ch_buffer[ch_car+1] == ' ' || ch_buffer[ch_car+1] == '\0')){ /* Mult case*/
             opr_buffer[opr_car++] = '*';
             ch_car += 2;
             continue;
         }
-        if (ch_buffer[ch_car] == '/' && (ch_buffer[ch_car+1] == ' ' || ch_buffer[ch_car+1] == '\0')){ /* Div case*/
+        else if (ch_buffer[ch_car] == '/' && (ch_buffer[ch_car+1] == ' ' || ch_buffer[ch_car+1] == '\0')){ /* Div case*/
             opr_buffer[opr_car++] = '/';
             ch_car += 2;
             continue;
         }
-        
+        else{
+             if (valid(ch_buffer+ch_car)){
+                 opn_buffer[opn_car++] = atof(ch_buffer+ch_car);
+             }
+             else
+                 return -1;
+        }        
     }
     return 0;
     
